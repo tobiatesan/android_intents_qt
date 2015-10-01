98b8fb92db9d1b8f9912ea4803a4bf9d4f77f8e1
Tobia Tesan <tobia.tesan@gmail.com>
2015-10-01 20:16:56 +0200

    Add URIHandler and openUri

--- qevent_test/main.cpp
+++ qevent_test/main.cpp
@@ -1,5 +1,14 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <jni.h>
+#include <QDebug>
+
+class URIHandler : public QObject {
+public:
+    void openUri(QString uri) {
+        qDebug() << "Open URI" << uri;
+    }
+} handler;
 
 int main(int argc, char *argv[])
 {
@@ -10,3 +19,24 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL
+  Java_com_foo_Bar_openUri(JNIEnv *env,
+                                                    jobject obj,
+                                                    jstring uri)
+{
+    jboolean isCopy;
+    isCopy = false;
+    const char* utf = env->GetStringUTFChars(uri, &isCopy);
+    handler.openUri(QString(utf));
+    env->ReleaseStringUTFChars(uri, utf);
+
+}
+
+#ifdef __cplusplus
+}
+#endif

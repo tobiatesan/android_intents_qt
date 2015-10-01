7ad32ba971db1f499c91ed8c7cb10ae85c658c69
Tobia Tesan <tobia.tesan@gmail.com>
2015-10-01 20:13:13 +0200

    Revert "Register MyEventFilter"
    This reverts commit ab74bb96a048aa4d496b4751764cef6062a800fe.

--- qevent_test/main.cpp
+++ qevent_test/main.cpp
@@ -1,12 +1,9 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
-#include <myeventfilter.h>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
-    app.installEventFilter(new MyEventFilter());
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
